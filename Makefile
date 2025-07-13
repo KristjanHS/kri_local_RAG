9661822cd1e9b5be89decf3c1df1dabb55e3080b
Kristjan Sillmann <kristjan.h.s@gmail.com>
2025-07-13 09:58:55 +0300

    initial project structure

--- Makefile
+++
@@ -1,7 +0,0 @@
-.PHONY: phase0 phase1
-
-phase0:
-	cd phase0 && make
-
-phase1:
-	cd phase1 && make
--- RAG_app/RAG_app/delete_collection.py
+++
@@ -1,6 +0,0 @@
-import weaviate
-from config import COLLECTION_NAME
-
-client = weaviate.connect_to_local()
-client.collections.delete(COLLECTION_NAME)
-client.close()
